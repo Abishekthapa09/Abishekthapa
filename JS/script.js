a4cfbdb5b5410602d79524c5f4ad767d8a93835c
Abishekthapa09 <109749100+Abishekthapa09@users.noreply.github.com>
2022-07-23 12:38:46 +0545

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 
 const STAR_COLOR = '#66e5e2';
-const STAR_SIZE = 3;
+const STAR_SIZE = 4;
 const STAR_MIN_SCALE = 0.2;
 const OVERFLOW_THRESHOLD = 50;
-const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8;
+const STAR_COUNT = (window.innerWidth + window.innerHeight) /7;
 
 const canvas = document.querySelector('canvas'),
 context = canvas.getContext('2d');
